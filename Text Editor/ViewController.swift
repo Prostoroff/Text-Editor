a68309e5bccf092c9ab88103873f30cc8a1c23cb
Ivan Osipov <ivan.loft72@gmail.com>
2022-08-18 21:10:21 +0500

    Initial Commit

---
+++ Text Editor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Text Editor
+//
+//  Created by Иван Осипов on 18/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
